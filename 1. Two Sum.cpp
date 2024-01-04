520d4b818d720c58daf130c833556657af5c186e
Rupesh Verma <137335317+rupeshverma43721@users.noreply.github.com>
2024-01-04 23:20:19 +0530

    Update 1. Two Sum.cpp
    // updated code uploaded

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -19,3 +19,24 @@ public:
         return ans;
     }
 };
+
+
+// updated 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int>ans;
+        unordered_map<int,int>umap;
+        for(int i=0;i<n;i++){
+            auto itr=umap.find(target-nums[i]);
+            if(itr!=umap.end()){
+                ans.push_back(itr->second);
+                ans.push_back(i);
+                break;
+            }
+            umap[nums[i]]=i;
+        }
+        return ans;
+    }
+};
